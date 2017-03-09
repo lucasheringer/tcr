2732abb12c4811e5e57bb8a8f4223ce758051359
Lucas C. Heringer <lucash@br.ibm.com>
2017-03-09 20:40:38 -0300

    adding some checking rules and measureing hop ping delay

--- tcr.sh
+++ tcr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##starting counters
-counter=30
+counter=15
 i=1
 slowestHop=""
 temp=0.0
@@ -17,27 +17,34 @@ fi
 ##starting traceroute
 while [ $i -lt $counter ]
 do
-	hop=`ping -c 1 -t $i $1| grep -i from | cut -d " " -f 2`
-
-	if [ "$hop" != "bytes" ]
+	hop=`ping -c 1 -t $i $1|grep icmp_seq|grep -oP '\(\K[^)]+'`
+  host=`ping -c 1 -t $i $1|grep icmp_seq|cut -d " " -f 2`
+	if [ "$?" -eq 0 ]
 	then
-		result=`ping -c 1 $hop | grep from`
-		echo $result
+		result=`ping -c 1 $hop | grep from` >> /dev/null
+		#echo $result
 		time1=`echo $result | cut -d "=" -f 4|cut -d " " -f 1`
-		echo "outside"
-		#### IF not working
-		if (( $(echo "$time1" > "$temp" |bc -l) ))
+		#echo $time1 $temp
+		if [ "$host" != "bytes" ] && [ ! -z "$host" ]
 		then
-			echo "inside"
-			$temp=$time1
-			echo $time1 $temp
-			#$slowestHop=$hop $time1 ms
+			echo $i $host $time1 "ms"
 		fi
-		i=$[$i+1]
+		if [ ! -z "$time1" ]
+		then
+			if [ $(echo " $time1 > $temp" | bc) -eq 1 ]
+			then
+				temp=$time1
+				slowestHop="$hop $time1 ms"
+				#echo $time1 $temp $slowestHop
+			fi
+		else
+			echo "  *  *  *  *"
+		fi
 	else
-		result=`ping -c 1 $1 | grep from`
+		result=`ping -c 1 -t $i $1|grep icmp_seq` >> /dev/null
 		echo $result
-		time1=`echo $result | cut -d "=" -f 4|cut -d " " -f 1`
-		i=31
 	fi
+	i=$[$i+1]
 done
+
+echo "Slowest hop: "$slowestHop

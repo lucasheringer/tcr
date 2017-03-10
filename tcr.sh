3df64680e25e5e19078a9d3da6ff74922fa625b2
Lucas C. Heringer <lucash@br.ibm.com>
2017-03-09 22:13:18 -0300

    adding comments to the code

--- tcr.sh
+++ tcr.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+	#!/bin/bash
 
 ##starting counters
-counter=15
+counter=20
 i=1
 slowestHop=""
 temp=0.0
@@ -17,20 +17,26 @@ fi
 ##starting traceroute
 while [ $i -lt $counter ]
 do
+	### reading the hop ip and hostname
 	hop=`ping -c 1 -t $i $1|grep icmp_seq|grep -oP '\(\K[^)]+'`
   host=`ping -c 1 -t $i $1|grep icmp_seq|cut -d " " -f 2`
+
 	if [ "$?" -eq 0 ]
 	then
 		result=`ping -c 1 $hop | grep from` >> /dev/null
 		#echo $result
+		## reading ping time to hop
 		time1=`echo $result | cut -d "=" -f 4|cut -d " " -f 1`
 		#echo $time1 $temp
+		## checking if it's not empty
 		if [ "$host" != "bytes" ] && [ ! -z "$host" ]
 		then
+			### printing info
 			echo $i $host $time1 "ms"
 		fi
 		if [ ! -z "$time1" ]
 		then
+			###Checking and saving the slowest hop
 			if [ $(echo " $time1 > $temp" | bc) -eq 1 ]
 			then
 				temp=$time1
@@ -41,10 +47,14 @@ do
 			echo "  *  *  *  *"
 		fi
 	else
+		### geting and printing the last hop
 		result=`ping -c 1 -t $i $1|grep icmp_seq` >> /dev/null
 		echo $result
 	fi
+	### increasing the TTL ping value until reaches the counter.
 	i=$[$i+1]
 done
 
+###printing the slowest hop
+echo "#################################"
 echo "Slowest hop: "$slowestHop
